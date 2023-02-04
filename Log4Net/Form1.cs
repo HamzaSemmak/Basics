6b4ba0a653653c341803c9a3643757f3c03411f5
unknown <semmak.hamza@gmail.com>
2023-02-04 01:07:34 +0000

    init

---
+++ Log4Net/Form1.cs
@@ -0,0 +1,38 @@
+﻿using log4net;
+using log4net.Repository.Hierarchy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Log4Net
+{
+    public partial class Form1 : Form
+    {
+        ILog Logger = LoggerHelper.GetLogger(typeof(Form1));
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Logger.Debug("Start Application Hamza");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Logger.Debug($"Clicked button1_Click {sender} - {e} ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Logger.Debug($"Clicked button2_Click {sender} - {e} ");
+        }
+    }
+}
---
+++ Log4Net/LoggerHelper.cs
@@ -0,0 +1,92 @@
+﻿using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Core;
+using log4net.Layout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4Net
+{
+    internal class LoggerHelper
+    {
+        private static ILog logger;
+        private static ConsoleAppender ConsoleAppender;
+        private static FileAppender FileAppender;
+        private static RollingFileAppender RollingFileAppender;
+        private static string layout = "%date{dd-MM-yyyy-HH:mm:ss} [%class] [%level] [%method] - %message%newline";
+
+        public static string Layout { set => layout = value; }
+
+        private static PatternLayout GetPatternLayout()
+        {
+            var PatternLayout = new PatternLayout()
+            {
+                ConversionPattern = layout
+            };
+            PatternLayout.ActivateOptions();
+            return PatternLayout;
+        }
+
+        private static ConsoleAppender GetConsoleAppender()
+        {
+            var ConsoleAppender = new ConsoleAppender()
+            {
+                Name = "ConsoleAppender",
+                Layout = GetPatternLayout(),
+                Threshold = Level.Error
+            };
+            ConsoleAppender.ActivateOptions();
+            return ConsoleAppender;
+        }
+
+        private static FileAppender GetFileAppender()
+        {
+            var FileAppender = new FileAppender()
+            {
+                Name = "fileAppender",
+                Layout = GetPatternLayout(),
+                Threshold = Level.All,
+                AppendToFile = false,
+                File = "Logs/FileAppender.log"
+            };
+            FileAppender.ActivateOptions();
+            return FileAppender;
+        }
+
+        private static RollingFileAppender GetRollingFileAppender()
+        {
+            var RollingFileAppender = new RollingFileAppender()
+            {
+                Name = "Rolling File Appender",
+                AppendToFile = true,
+                File = "Logs/RollingFileAppender.log",
+                Layout = GetPatternLayout(),
+                Threshold = Level.All,
+                MaximumFileSize = "1MB",
+                MaxSizeRollBackups = 15
+            };
+            RollingFileAppender.ActivateOptions();
+            return RollingFileAppender;
+        }
+
+        public static ILog GetLogger(Type type)
+        {
+            if (ConsoleAppender == null)
+                ConsoleAppender = GetConsoleAppender();
+            if(FileAppender == null)
+                FileAppender = GetFileAppender();
+            if(RollingFileAppender == null)
+                RollingFileAppender = GetRollingFileAppender();
+            if (logger != null)
+                return logger;
+
+            BasicConfigurator.Configure(ConsoleAppender, FileAppender, RollingFileAppender);
+            logger = LogManager.GetLogger(type);
+            return logger;
+        }
+    }
+}
