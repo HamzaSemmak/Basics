03a4d98ad5cf5312966c258a63392c38ba9288b9
unknown <semmak.hamza@gmail.com>
2023-02-10 23:41:23 +0000

    init

---
+++ Point Of Sale/LoggerHelper.cs
@@ -0,0 +1,76 @@
+﻿using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Core;
+using log4net.Layout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4Net
+{
+    internal class LoggerHelper
+    {
+        private static ILog logger;
+        private static ConsoleAppender ConsoleAppender;
+        private static RollingFileAppender RollingFileAppender;
+        private static string layout = "%date [%thread] [%-5level] %logger: %message%newline";
+
+        public static string Layout { set => layout = value; }
+
+        private static PatternLayout GetPatternLayout()
+        {
+            var PatternLayout = new PatternLayout()
+            {
+                ConversionPattern = layout
+            };
+            PatternLayout.ActivateOptions();
+            return PatternLayout;
+        }
+
+        private static ConsoleAppender GetConsoleAppender()
+        {
+            var ConsoleAppender = new ConsoleAppender()
+            {
+                Name = "ConsoleAppender",
+                Layout = GetPatternLayout(),
+                Threshold = Level.Error
+            };
+            ConsoleAppender.ActivateOptions();
+            return ConsoleAppender;
+        }
+
+        private static RollingFileAppender GetRollingFileAppender()
+        {
+            var RollingFileAppender = new RollingFileAppender()
+            {
+                Name = "Rolling File Appender",
+                Layout = GetPatternLayout(),
+                Threshold = Level.All,
+                MaximumFileSize = "1MB",
+                MaxSizeRollBackups = 15,
+                StaticLogFileName = false,
+                AppendToFile = true,
+                File = "Logs/",
+            };
+            RollingFileAppender.ActivateOptions();
+            return RollingFileAppender;
+        }
+
+        public static ILog GetLogger(Type type)
+        {
+            if (ConsoleAppender == null)
+                ConsoleAppender = GetConsoleAppender();
+            if(RollingFileAppender == null)
+                RollingFileAppender = GetRollingFileAppender();
+            if (logger != null)
+                return logger;
+
+            BasicConfigurator.Configure(ConsoleAppender, RollingFileAppender);
+            logger = LogManager.GetLogger(type);
+            return logger;
+        }
+    }
+}
---
+++ Point Of Sale/Program.cs
@@ -0,0 +1,23 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Point_Of_Sale
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PrincipalForm());
+        }
+    }
+}
