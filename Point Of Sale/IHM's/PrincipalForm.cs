bea7d2f364607dc87c1d2cf48ddc748200cf0f1a
HAMZA SEMMAK <hsemmak@sorec.ma>
2023-02-14 20:56:50 +0100

    init

--- Point Of Sale/IHM's/PrincipalForm.cs
+++ Point Of Sale/IHM's/PrincipalForm.cs
@@ -15,7 +15,13 @@ namespace Point_Of_Sale.IHM_s
         public PrincipalForm(string UserName)
         {
             InitializeComponent();
-            //label1.Text = UserName.ToString();
+            label2.Text = UserName.ToString();
+            label3.Text = $"{DateTime.Now.Day.ToString()}/{DateTime.Now.Month.ToString()}/{DateTime.Now.Year.ToString()}";
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            Program.CloseApp();
         }
     }
 }
