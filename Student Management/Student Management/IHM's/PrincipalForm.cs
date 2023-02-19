af2e7daf127a9a239eefdfed832e3b690b3ece80
HAMZA SEMMAK <hsemmak@sorec.ma>
2023-02-19 17:25:41 +0100

    init

--- Student Management/Student Management/IHM's/PrincipalForm.cs
+++ Student Management/Student Management/IHM's/PrincipalForm.cs
@@ -17,11 +17,28 @@ namespace Student_Management.IHM_s
         {
             InitializeComponent();
             label1.Text = $"{dateTime.DayOfWeek}, {dateTime.Day} {dateTime.ToString("MMMM")} {dateTime.Year}";
+
+            //Test 
+            String[] arr1 = new string[] { "1", "Hamza Semmak", "21", "21" };
+            String[] arr2 = new string[] { "1", "Hamza Semmak", "21", "21" };
+            String[] arr3 = new string[] { "1", "Hamza Semmak", "21", "21" };
+            String[] arr4 = new string[] { "1", "Hamza Semmak", "21", "21" };
+            Object[] rows = new object[] { arr1, arr2, arr3, arr4 };
+            foreach (string[] rowArr in rows)
+            {
+                this.guna2DataGridView1.Rows.Add(rowArr);
+            }
         }
 
         private void guna2Button1_Click(object sender, EventArgs e)
         {
             Program.CloseApplication();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Program.LaunchPrincipalForm();
         }
     }
 }
