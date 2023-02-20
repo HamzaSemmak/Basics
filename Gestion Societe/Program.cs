3234d080cd6716cd01b020df9158d83d7f1d0c29
HAMZA SEMMAK <hsemmak@sorec.ma>
2023-02-20 16:47:22 +0100

    init

--- Gestion Societe/Program.cs
+++ Gestion Societe/Program.cs
@@ -8,9 +8,23 @@ namespace Gestion_Societe
 {
     internal class Program
     {
+        public static string GeneratePassword(int passLength)
+        {
+            var chars = "abcdefghijklmnopqrstuvwxyz@#$&ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            var result = new string(
+                Enumerable.Repeat(chars, passLength)
+                          .Select(s => s[random.Next(s.Length)])
+                          .ToArray());
+            return result;
+        }
         static void Main(string[] args)
         {
-
+            Console.WriteLine(GeneratePassword(10));
+            Console.WriteLine(GeneratePassword(10));
+            Console.WriteLine(GeneratePassword(10));
+            Console.WriteLine(GeneratePassword(10));
+            Console.WriteLine(GeneratePassword(10));
         }
     }
 }
