89f198b8bda9d842778e546a77f9a7932192dead
unknown <semmak.hamza@gmail.com>
2023-06-01 23:57:42 +0100

    init

---
+++ Angular/Authentication/src/Module/Route.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from '@angular/router';
+
+const Route: Routes = [];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(Route) ],
+    exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {  }
--- Angular/Authentication/src/app/app.module.ts
+++ Angular/Authentication/src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/Module/Material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { RegisterComponent } from './Component/register/register.component';
+import { LoginComponent } from './Component/login/login.component';
+import { HomeComponent } from './Component/home/home.component';
+import { UsersComponent } from './Component/users/users.component';
+import { UpdatePopUpComponent } from './Component/update-pop-up/update-pop-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    UsersComponent,
+    UpdatePopUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
