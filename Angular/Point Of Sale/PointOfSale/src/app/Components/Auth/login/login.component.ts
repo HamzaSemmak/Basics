65cbb26dca191cbc37480576bd351164f80ae437
HAMZA SEMMAK <hsemmak@sorec.ma>
2023-06-09 16:32:26 +0100

    init

--- Angular/Point Of Sale/PointOfSale/src/app/Components/Auth/login/login.component.ts
+++ Angular/Point Of Sale/PointOfSale/src/app/Components/Auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,7 +14,25 @@ export class LoginComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-
+    var owl = $('.owl-carousel');
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      item:1,
+      dots: false,
+      margin:0,
+      autoWidth:true,
+      autoplay:true,
+      autoplayTimeout:5000,
+      smartSpeed:450
+    });
+    owl.on('mousewheel', '.owl-stage', function (e: any) {
+        if (e.deltaY>0) {
+            owl.trigger('next.owl');
+        } else {
+            owl.trigger('prev.owl');
+        }
+        e.preventDefault();
+    });
   }
 
 }
