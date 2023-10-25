c580a2f702b7163f4aa5d3eedd1f2f8a5d686541
HamzaSemmak <semmak.hamza@gmail.com>
2023-10-25 18:46:19 +0100

    init

---
+++ Java/learn-spring-data-jpa/src/main/java/com/learn/learnspringdatajpa/entity/Student.java
@@ -0,0 +1,50 @@
+package com.learn.learnspringdatajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(
+        name = "student",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "email_unique", columnNames = "student_email")
+        }
+)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @Column(name = "id")
+    private Long studentId;
+
+    @Column(name = "student_first_name")
+    private String firstName;
+
+    @Column(name = "student_last_name")
+    private String lastName;
+
+    @Column(name = "student_email", nullable = false)
+    private String emailId;
+
+    @Column(name = "guardian_name")
+    private String guardianName;
+
+    @Column(name = "guardian_email")
+    private String guardianEmail;
+
+    @Column(name = "guardian_mobile")
+    private String guardianMobile;
+
+}
