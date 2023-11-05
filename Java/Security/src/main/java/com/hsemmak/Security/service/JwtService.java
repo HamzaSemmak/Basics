d9d351b2f1309efad501e4c26c5450cadc4b3b7a
HamzaSemmak <semmak.hamza@gmail.com>
2023-11-05 18:39:00 +0100

    init

--- Java/Security/src/main/java/com/hsemmak/Security/service/JwtService.java
+++ Java/Security/src/main/java/com/hsemmak/Security/service/JwtService.java
@@ -1,10 +1,22 @@
 package com.hsemmak.Security.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
-    public String extractUserName(String jwt) {
+    public String extractUserName(String token) {
         return null;
+    }
+
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
     }
 }
