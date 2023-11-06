47c0e2955f3ff89c847d5c77c8173d5e062de23d
HamzaSemmak <semmak.hamza@gmail.com>
2023-11-06 17:00:53 +0100

    init

--- Java/Learns/Security/src/main/java/com/hsemmak/Security/config/JwtAuthenticationFilter.java
+++ Java/Learns/Security/src/main/java/com/hsemmak/Security/config/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.hsemmak.Security.config;
 
 import com.hsemmak.Security.service.JwtService;
+import com.hsemmak.Security.service.UserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,6 +9,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -19,6 +23,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtService jwtService;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -37,5 +44,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         jwt = Header.substring(7);
         userEmail = jwtService.extractUserName(jwt);
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadByUserName(userEmail);
+        }
     }
 }
---
+++ Java/Learns/Security/src/main/java/com/hsemmak/Security/service/UserDetailsService.java
@@ -0,0 +1,11 @@
+package com.hsemmak.Security.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsService {
+    public UserDetails loadByUserName(String userEmail) {
+
+    }
+}
